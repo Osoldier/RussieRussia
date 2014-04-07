edadddab3da67a8c415f13d3927fc89f4be3be5d
IBANEZT_INFO <IBANEZT_INFO@10.134.97.159>
2014-04-07 15:59:59 +0200

    Props

--- public_html/files/javascript/maps.js
+++ public_html/files/javascript/maps.js
@@ -1,4 +1,21 @@
-var 
+var TAPIS = 0;
+var TABLE = 1;
+var TABLEAU = 2;
+var PORTE = 3;
+var CLEF = 4;
+var PUIT = 5;
+var MUR = 6;
+var TANK = 7;
+var DRAPCOM = 8;
+var LIVRE = 9;
+var OBAMA = 10;
+var BENLADEN = 11;
+var YANOUKOVIC = 12;
+var FEMINISTE = 13;
+var OURS = 14;
+var VODKA = 15;
+var CAVIAR = 16;
+var CIGARETTE = 17;
 
 function room(id)  {
     this.id = id;
