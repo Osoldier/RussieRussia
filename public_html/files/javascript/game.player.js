0014600404032430c02078dfe7c9ec4aa15c0e95
ESTEBAN <>
2014-04-08 11:06:10 +0200

    UPDATE

--- public_html/files/javascript/game.player.js
+++ public_html/files/javascript/game.player.js
@@ -1,6 +1,6 @@
 var player = new player();
 
-function player(){
+function player() {
     this.room = 0;
     this.time = 0;
     this.score = 0;
@@ -11,5 +11,29 @@ function player(){
     this.x = 0;
     this.y = 0;
     this.speed = 10;
+    this.direction = 0;
     this.type = 0;
+    this.frame = 0;
+    this.Move = function() {
+        if (KeyState.w || KeyState.up)
+        {
+            this.direction = Direction.UP;
+        }
+        if (KeyState.d || KeyState.right)
+        {
+            this.direction = Direction.RIGHT;
+        }
+        if (KeyState.s || KeyState.down)
+        {
+            this.direction = Direction.DOWN;
+        }
+        if (KeyState.a || KeyState.left)
+        {
+            this.direction = Direction.LEFT;
+        }
+    };
+
+    this.Afficher = function() {
+
+    };
 }
