5ee49beaf0ca3f1525b2bd7bbb33777f8742d410
ESTEBAN <>
2014-04-08 09:03:30 +0200

    UPDATE

--- public_html/files/javascript/game.player.js
+++ public_html/files/javascript/game.player.js
@@ -1,12 +1,15 @@
-var player = {
-    'room': 0,
-    'time': 0,
-    'score': 0,
-    'object1': 0,
-    'object2': 0,
-    'object3': 0,
-    'life': 0,
-    'x': 0,
-    'y': 0,
-    'speed':0
-};
+var player = new player();
+
+function player(){
+    this.room = 0;
+    this.time = 0;
+    this.score = 0;
+    this.object1 = 0;
+    this.object2 = 0;
+    this.object3 = 0;
+    this.life = 0;
+    this.x = 0;
+    this.y = 0;
+    this.speed = 10;
+    this.type = 0;
+}
