1a3e7f50f229be0d1dd18adba5e1329f6d778148
Osoldier (Carlos) <>
2014-04-16 15:08:49 +0200

    blablabla

--- public_html/files/javascript/game.score.js
+++ public_html/files/javascript/game.score.js
@@ -2,6 +2,9 @@ var score = new score();
 
 function score() {
     this.value = 0;
+    this.hunger = 100;
+    this.
+    this.inventory = {};
     this.canvas = null;
     this.context = null;
     this.background = new Image();
@@ -15,6 +18,21 @@ function score() {
     this.Update = function()
     {
         this.context.drawImage(this.background,0,0,200,768);
+        //RUSSIE
+        this.context.fillStyle = "white"
+        this.context.beginPath();
+        this.context.rect(0, 200-this.hunger, 10,this.hunger);
+        this.context.fill();
+        this.context.fillStyle = "darkblue"
+        this.context.beginPath();
+        this.context.rect(10, 200-this.hunger, 10,this.hunger);
+        this.context.fill();
+        this.context.fillStyle = "red"
+        this.context.beginPath();
+        this.context.rect(20, 200-this.hunger, 10,this.hunger);
+        this.context.fill();
+        //FIN_RUSSIE
+        //UKRAINE
     };
 };
 
