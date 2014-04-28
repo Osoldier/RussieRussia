dc1c34c72ca0f4c6641e023e203c5a0237ab5465
Osoldier <tom.basket@hotmail.com>
2014-04-28 13:43:29 +0200

    blablabla

--- public_html/files/javascript/game.weapons.js
+++ public_html/files/javascript/game.weapons.js
@@ -37,8 +37,9 @@ function vodka(x, y, dir) {
 function faucile(x, y, dir) {
     this.x = x;
     this.y = y;
-    this.width = 15;
-    this.height = 30;
+    this.width = 30;
+    this.height = 45;
+    this.theta = 0;
     this.sprite = new Image();
     this.sprite.src = "files/images/Faucille.png";
     this.dir = dir;
