fdbcff35747d236f7f5b4593dc3fd0e464660926
Osoldier <tom.basket@hotmail.com>
2014-04-28 13:40:46 +0200

    blablabla

--- public_html/files/javascript/game.weapons.js
+++ public_html/files/javascript/game.weapons.js
@@ -71,8 +71,9 @@ function faucile(x, y, dir) {
 function machette(x, y, dir) {
     this.x = x;
     this.y = y;
-    this.width = 15;
-    this.height = 30;
+    this.width = 30;
+    this.height = 45;
+    this.theta = 0;
     this.sprite = new Image();
     this.sprite.src = "files/images/Machette.png";
     this.dir = dir;
