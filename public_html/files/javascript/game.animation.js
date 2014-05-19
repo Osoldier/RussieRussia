41269843b7e613b221aa4a85cd59f16c4befd437
KOUKOU <>
2014-05-19 13:22:40 +0200

    ...

--- public_html/files/javascript/game.animation.js
+++ public_html/files/javascript/game.animation.js
@@ -9,19 +9,20 @@ var CURRENTANIMATION = 0;
 var FALL = 0;
 var UPSTAIR = 1;
 var DOWNSTAIR = 2;
+var THEOREMEDELAPIZZA = 3;
 var Time = 0;
 
 function fall() {
-     Game.context.beginPath();
-        Game.context.fillStyle = "black";
-        Game.context.rect(0,0,Game.canvas.width,Game.canvas.height);
-        Game.context.fill();
-    for (var i = 0; i <= Time/27; i++) {
-        var vel = 4*(Time-(i*27));
+    Game.context.beginPath();
+    Game.context.fillStyle = "black";
+    Game.context.rect(0, 0, Game.canvas.width, Game.canvas.height);
+    Game.context.fill();
+    for (var i = 0; i <= Time / 27; i++) {
+        var vel = 4 * (Time - (i * 27));
         Game.context.beginPath();
         Game.context.strokeStyle = "white";
         Game.context.lineWidth = 1;
-        Game.context.rect(Game.canvas.width/2-vel, Game.canvas.height/2-vel, 2*vel, 2*vel);
+        Game.context.rect(Game.canvas.width / 2 - vel, Game.canvas.height / 2 - vel, 2 * vel, 2 * vel);
         Game.context.stroke();
     }
 }
@@ -31,6 +32,10 @@ function UpStair() {
 }
 
 function DownStair() {
+
+}
+
+function Pizza() {
 
 }
 
@@ -45,6 +50,9 @@ function UpdateAnimation() {
             break;
         case DOWNSTAIR:
             DownStair();
+            break;
+        case THEOREMEDELAPIZZA:
+            Pizza();
             break;
     }
     if (Time >= 100) {
