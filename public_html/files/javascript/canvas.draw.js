37e228f73fca084231f667516cb2ce92a5d9f252
KOUKOU <>
2014-05-19 09:26:35 +0200

    ...

--- public_html/files/javascript/canvas.draw.js
+++ public_html/files/javascript/canvas.draw.js
@@ -39,7 +39,7 @@ function Canvas()
             x = player.roomInfo[0];
             while (x < player.roomInfo[0] + player.roomInfo[2])
             {
-                Game.context.drawImage(Images["border_door_ground"], 142, 824, 50, 50, x, y, 50, 50);
+                Game.context.drawImage(Images['borderDoorGround'], 142, 824, 50, 50, x, y, 50, 50);
                 x = x + 50;
             }
             y = y + 50;
@@ -64,15 +64,15 @@ function Canvas()
         var y = player.roomInfo[1];
         //#####Salle carrée#####//
         if (player.roomInfo[2] == 700 && player.roomInfo[3] == 700) //SQUARED        
-            Game.context.drawImage(Images['border_door_ground'], 1566, 0, 880, 882, x - 91, y - 91, 880, 882);
+            Game.context.drawImage(Images['borderDoorGround'], 1566, 0, 880, 882, x - 91, y - 91, 880, 882);
 
         //#####Salle longue#####//
         if (player.roomInfo[2] == 500 && player.roomInfo[3] == 700) //LONGER
-            Game.context.drawImage(Images['border_door_ground'], 884, 0, 682, 884, x - 91, y - 91, 682, 884);
+            Game.context.drawImage(Images['borderDoorGround'], 884, 0, 682, 884, x - 91, y - 91, 682, 884);
 
         //#####Salle large#####// 
         if (player.roomInfo[2] == 700 && player.roomInfo[3] == 500) //LARGER
-            Game.context.drawImage(Images['border_door_ground'], 0, 0, 884, 682, x - 92, y - 91, 884, 682);
+            Game.context.drawImage(Images['borderDoorGround'], 0, 0, 884, 682, x - 92, y - 91, 884, 682);
     };
 
 //##############################################################################
@@ -89,7 +89,7 @@ function Canvas()
                 case TOP:
                 case TOPLEFT:
                 case TOPRIGHT:
-                    Game.context.drawImage(Images['border_door_ground'], 0, 682, 142, 91, entry.imgX, entry.imgY, entry.imgwidth, entry.imgHeight);
+                    Game.context.drawImage(Images['borderDoorGround'], 0, 682, 142, 91, entry.imgX, entry.imgY, entry.imgwidth, entry.imgHeight);
                     /* HITZONE 
                      y = roomY;
                      x = roomX + (map[player.room].width / 2) - (entry.width / 2);
@@ -101,42 +101,42 @@ function Canvas()
                     if (entry.lock) {
                         switch (entry.color) {
                             case RED:
-                                Game.context.drawImage(Images['border_door_ground'], 433, 683, lock.width, lock.height, entry.x + (lock.width / 2), entry.y - (lock.height / 2) - 5, lock.width, lock.height);
+                                Game.context.drawImage(Images['borderDoorGround'], 433, 683, lock.width, lock.height, entry.x + (lock.width / 2), entry.y - (lock.height / 2) - 5, lock.width, lock.height);
                                 break;
                             case BLUE:
-                                Game.context.drawImage(Images['border_door_ground'], 544, 683, lock.width, lock.height, entry.x + (lock.width / 2), entry.y - (lock.height / 2) - 5, lock.width, lock.height);
+                                Game.context.drawImage(Images['borderDoorGround'], 544, 683, lock.width, lock.height, entry.x + (lock.width / 2), entry.y - (lock.height / 2) - 5, lock.width, lock.height);
                                 break;
                             case GREEN:
-                                Game.context.drawImage(Images['border_door_ground'], 655, 683, lock.width, lock.height, entry.x + (lock.width / 2), entry.y - (lock.height / 2) - 5, lock.width, lock.height);
+                                Game.context.drawImage(Images['borderDoorGround'], 655, 683, lock.width, lock.height, entry.x + (lock.width / 2), entry.y - (lock.height / 2) - 5, lock.width, lock.height);
                                 break;
                             case YELLOW:
-                                Game.context.drawImage(Images['border_door_ground'], 766, 683, lock.width, lock.height, entry.x + (lock.width / 2), entry.y - (lock.height / 2) - 5, lock.width, lock.height);
+                                Game.context.drawImage(Images['borderDoorGround'], 766, 683, lock.width, lock.height, entry.x + (lock.width / 2), entry.y - (lock.height / 2) - 5, lock.width, lock.height);
                                 break;
                             case null:
-                                Game.context.drawImage(Images['border_door_ground'], 322, 683, lock.width, lock.height, entry.x + (lock.width / 2), entry.y - (lock.height / 2) - 5, lock.width, lock.height);
+                                Game.context.drawImage(Images['borderDoorGround'], 322, 683, lock.width, lock.height, entry.x + (lock.width / 2), entry.y - (lock.height / 2) - 5, lock.width, lock.height);
                                 break;
                         }
                     }
                     break;
                 case LEFT:
-                    Game.context.drawImage(Images['border_door_ground'], 233, 683, 85, 140, entry.imgX, entry.imgY, entry.imgwidth, entry.imgHeight);
+                    Game.context.drawImage(Images['borderDoorGround'], 233, 683, 85, 140, entry.imgX, entry.imgY, entry.imgwidth, entry.imgHeight);
 
                     if (entry.lock) {
                         switch (entry.color) {
                             case RED:
-                                Game.context.drawImage(Images['border_door_ground'], 486, 741, lock.height, lock.width, entry.x - (lock.height / 2), entry.y + (lock.width / 2), lock.height, lock.width);
+                                Game.context.drawImage(Images['borderDoorGround'], 486, 741, lock.height, lock.width, entry.x - (lock.height / 2), entry.y + (lock.width / 2), lock.height, lock.width);
                                 break;
                             case BLUE:
-                                Game.context.drawImage(Images['border_door_ground'], 597, 741, lock.height, lock.width, entry.x - (lock.height / 2), entry.y + (lock.width / 2), lock.height, lock.width);
+                                Game.context.drawImage(Images['borderDoorGround'], 597, 741, lock.height, lock.width, entry.x - (lock.height / 2), entry.y + (lock.width / 2), lock.height, lock.width);
                                 break;
                             case GREEN:
-                                Game.context.drawImage(Images['border_door_ground'], 708, 741, lock.height, lock.width, entry.x - (lock.height / 2), entry.y + (lock.width / 2), lock.height, lock.width);
+                                Game.context.drawImage(Images['borderDoorGround'], 708, 741, lock.height, lock.width, entry.x - (lock.height / 2), entry.y + (lock.width / 2), lock.height, lock.width);
                                 break;
                             case YELLOW:
-                                Game.context.drawImage(Images['border_door_ground'], 819, 741, lock.height, lock.width, entry.x - (lock.height / 2), entry.y + (lock.width / 2), lock.height, lock.width);
+                                Game.context.drawImage(Images['borderDoorGround'], 819, 741, lock.height, lock.width, entry.x - (lock.height / 2), entry.y + (lock.width / 2), lock.height, lock.width);
                                 break;
                             case null:
-                                Game.context.drawImage(Images['border_door_ground'], 375, 741, lock.height, lock.width, entry.x - (lock.height / 2), entry.y + (lock.width / 2), lock.height, lock.width);
+                                Game.context.drawImage(Images['borderDoorGround'], 375, 741, lock.height, lock.width, entry.x - (lock.height / 2), entry.y + (lock.width / 2), lock.height, lock.width);
                                 break;
                         }
                     }
@@ -144,48 +144,48 @@ function Canvas()
                 case BOTTOM:
                 case BOTLEFT:
                 case BOTRIGHT:
-                    Game.context.drawImage(Images['border_door_ground'], 0, 773, 142, 91, entry.imgX, entry.imgY, entry.imgwidth, entry.imgHeight);
+                    Game.context.drawImage(Images['borderDoorGround'], 0, 773, 142, 91, entry.imgX, entry.imgY, entry.imgwidth, entry.imgHeight);
 
                     if (entry.lock) {
 
                         switch (entry.color) {
                             case RED:
-                                Game.context.drawImage(Images['border_door_ground'], 433, 741, 53, 58, entry.x + (lock.width / 2), entry.y, lock.width, lock.height);
+                                Game.context.drawImage(Images['borderDoorGround'], 433, 741, 53, 58, entry.x + (lock.width / 2), entry.y, lock.width, lock.height);
                                 break;
                             case BLUE:
-                                Game.context.drawImage(Images['border_door_ground'], 544, 741, 53, 58, entry.x + (lock.width / 2), entry.y, lock.width, lock.height);
+                                Game.context.drawImage(Images['borderDoorGround'], 544, 741, 53, 58, entry.x + (lock.width / 2), entry.y, lock.width, lock.height);
                                 break;
                             case GREEN:
-                                Game.context.drawImage(Images['border_door_ground'], 655, 741, 53, 58, entry.x + (lock.width / 2), entry.y, lock.width, lock.height);
+                                Game.context.drawImage(Images['borderDoorGround'], 655, 741, 53, 58, entry.x + (lock.width / 2), entry.y, lock.width, lock.height);
                                 break;
                             case YELLOW:
-                                Game.context.drawImage(Images['border_door_ground'], 766, 741, 53, 58, entry.x + (lock.width / 2), entry.y, lock.width, lock.height);
+                                Game.context.drawImage(Images['borderDoorGround'], 766, 741, 53, 58, entry.x + (lock.width / 2), entry.y, lock.width, lock.height);
                                 break;
                             case null:
-                                Game.context.drawImage(Images['border_door_ground'], 322, 741, 53, 58, entry.x + (lock.width / 2), entry.y, lock.width, lock.height);
+                                Game.context.drawImage(Images['borderDoorGround'], 322, 741, 53, 58, entry.x + (lock.width / 2), entry.y, lock.width, lock.height);
                                 break;
                         }
                     }
                     break;
                 case RIGHT:
-                    Game.context.drawImage(Images['border_door_ground'], 142, 683, 91, 140, entry.imgX, entry.imgY, entry.imgwidth, entry.imgHeight);
+                    Game.context.drawImage(Images['borderDoorGround'], 142, 683, 91, 140, entry.imgX, entry.imgY, entry.imgwidth, entry.imgHeight);
 
                     if (entry.lock) {
                         switch (entry.color) {
                             case RED:
-                                Game.context.drawImage(Images['border_door_ground'], 486, 683, lock.height, lock.width, entry.x, entry.y + (lock.width / 2), lock.height, lock.width);
+                                Game.context.drawImage(Images['borderDoorGround'], 486, 683, lock.height, lock.width, entry.x, entry.y + (lock.width / 2), lock.height, lock.width);
                                 break;
                             case BLUE:
-                                Game.context.drawImage(Images['border_door_ground'], 597, 683, lock.height, lock.width, entry.x, entry.y + (lock.width / 2), lock.height, lock.width);
+                                Game.context.drawImage(Images['borderDoorGround'], 597, 683, lock.height, lock.width, entry.x, entry.y + (lock.width / 2), lock.height, lock.width);
                                 break;
                             case GREEN:
-                                Game.context.drawImage(Images['border_door_ground'], 708, 683, lock.height, lock.width, entry.x, entry.y + (lock.width / 2), lock.height, lock.width);
+                                Game.context.drawImage(Images['borderDoorGround'], 708, 683, lock.height, lock.width, entry.x, entry.y + (lock.width / 2), lock.height, lock.width);
                                 break;
                             case YELLOW:
-                                Game.context.drawImage(Images['border_door_ground'], 819, 683, lock.height, lock.width, entry.x, entry.y + (lock.width / 2), lock.height, lock.width);
+                                Game.context.drawImage(Images['borderDoorGround'], 819, 683, lock.height, lock.width, entry.x, entry.y + (lock.width / 2), lock.height, lock.width);
                                 break;
                             case null:
-                                Game.context.drawImage(Images['border_door_ground'], 375, 683, lock.height, lock.width, entry.x, entry.y + (lock.width / 2), lock.height, lock.width);
+                                Game.context.drawImage(Images['borderDoorGround'], 375, 683, lock.height, lock.width, entry.x, entry.y + (lock.width / 2), lock.height, lock.width);
                                 break;
                         }
                     }
